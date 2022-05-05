a3ac6d480f6011bf74f84cb85eaeb9bcc7674f54
jitu2404 <jatin.ahluwalia.5@gmail.com>
2022-05-05 19:00:54 +0530

    .

--- confirm.js
+++ confirm.js
@@ -1,1 +1,6 @@
-document.getElementById("congo").innerText += " " + localStorage.getItem("myname");
+document.getElementById("congo").innerText += " " + localStorage.getItem("myname"); 
+document.getElementById("details").innerText +=  "Phone: " + localStorage.getItem("myphone")+"\n"; 
+document.getElementById("details").innerText +=  "Persons: " + localStorage.getItem("mypersons")+"\n"; 
+document.getElementById("details").innerText +=  "Date: " + localStorage.getItem("mydate")+"\n"; 
+document.getElementById("details").innerText +=  "Time: " + localStorage.getItem("mytime")+"\n"; 
+
--- reserve.js
+++ reserve.js
@@ -1,4 +1,14 @@
 function myfn() {
     var name = document.getElementById("fname").value;
+    var phone = document.getElementById('phn').value;
+    var date = document.getElementById('datee').value;
+    var persons  = document.getElementById("number").value; 
+    var time = document.getElementById("timee").value;
     localStorage.setItem("myname", name);
+    localStorage.setItem("myphone", phone);
+    localStorage.setItem("mydate", date)
+    localStorage.setItem("mypersons", persons);
+    localStorage.setItem("mytime", time);
+
+
 }
