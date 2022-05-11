9a567985398e3c3ec43f00f700ef6a27045eca35
jitu2404 <jatin.ahluwalia.5@gmail.com>
2022-05-11 18:27:21 +0530

    1

--- confirm.js
+++ confirm.js
@@ -1,4 +1,4 @@
-document.getElementById("congo").innerText += " " + localStorage.getItem("myname"); 
+document.getElementById("congo").innerText += " " + localStorage.getItem("myname").charAt(0).toUpperCase()+localStorage.getItem("myname").slice(1); 
 document.getElementById("details").innerText +=  "Phone: " + localStorage.getItem("myphone")+"\n"; 
 document.getElementById("details").innerText +=  "Persons: " + localStorage.getItem("mypersons")+"\n"; 
 document.getElementById("details").innerText +=  "Date: " + localStorage.getItem("mydate")+"\n"; 
